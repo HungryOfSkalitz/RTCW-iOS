ee8e4ec07ad7ec82714d7c89c8e0456713715d87
HungryOfSkalitz <arsenijbogdanov395@gmail.com>
2026-07-07 08:28:49 +0400

    Trying to Update SDL_uikitviewcontroller+Additions.swift

--- RTCW-SP-iOS/SDL_uikitviewcontroller+Additions.swift
+++ RTCW-SP-iOS/SDL_uikitviewcontroller+Additions.swift
@@ -1,4 +1,4 @@
-//
+/
 //  SDL2ViewController+Additions.swift
 //  Quake2-iOS
 //
@@ -32,156 +32,88 @@ extension SDL_uikitviewcontroller {
     }
     
     var fireButton:UIButton {
-        get {
-            return Holder._fireButton
-        }
-        set(newValue) {
-            Holder._fireButton = newValue
-        }
+        get { return Holder._fireButton }
+        set(newValue) { Holder._fireButton = newValue }
     }
     
     var jumpButton:UIButton {
-        get {
-            return Holder._jumpButton
-        }
-        set(newValue) {
-            Holder._jumpButton = newValue
-        }
+        get { return Holder._jumpButton }
+        set(newValue) { Holder._jumpButton = newValue }
     }
     
     var useButton:UIButton {
-        get {
-            return Holder._useButton
-        }
-        set(newValue) {
-            Holder._useButton = newValue
-        }
+        get { return Holder._useButton }
+        set(newValue) { Holder._useButton = newValue }
     }
     
     var crouchButton:UIButton {
-        get {
-            return Holder._crouchButton
-        }
-        set(newValue) {
-            Holder._crouchButton = newValue
-        }
+        get { return Holder._crouchButton }
+        set(newValue) { Holder._crouchButton = newValue }
     }
     
     var joystickView:JoyStickView {
-        get {
-            return Holder._joystickView
-        }
-        set(newValue) {
-            Holder._joystickView = newValue
-        }
+        get { return Holder._joystickView }
+        set(newValue) { Holder._joystickView = newValue }
     }
 
     var tildeButton:UIButton {
-        get {
-            return Holder._tildeButton
-        }
-        set(newValue) {
-            Holder._tildeButton = newValue
-        }
+        get { return Holder._tildeButton }
+        set(newValue) { Holder._tildeButton = newValue }
     }
 
     var escapeButton:UIButton {
-        get {
-            return Holder._escapeButton
-        }
-        set(newValue) {
-            Holder._escapeButton = newValue
-        }
+        get { return Holder._escapeButton }
+        set(newValue) { Holder._escapeButton = newValue }
     }
 
     var expandButton:UIButton {
-        get {
-            return Holder._expandButton
-        }
-        set(newValue) {
-            Holder._expandButton = newValue
-        }
+        get { return Holder._expandButton }
+        set(newValue) { Holder._expandButton = newValue }
     }
     
     var quickLoadButton:UIButton {
-        get {
-            return Holder._quickLoadButton
-        }
-        set(newValue) {
-            Holder._quickLoadButton = newValue
-        }
+        get { return Holder._quickLoadButton }
+        set(newValue) { Holder._quickLoadButton = newValue }
     }
     
     var quickSaveButton:UIButton {
-        get {
-            return Holder._quickSaveButton
-        }
-        set(newValue) {
-            Holder._quickSaveButton = newValue
-        }
+        get { return Holder._quickSaveButton }
+        set(newValue) { Holder._quickSaveButton = newValue }
     }
     
     var buttonStack:UIStackView {
-        get {
-            return Holder._buttonStack
-        }
-        set(newValue) {
-            Holder._buttonStack = newValue
-        }
+        get { return Holder._buttonStack }
+        set(newValue) { Holder._buttonStack = newValue }
     }
 
     var buttonStackExpanded:Bool {
-        get {
-            return Holder._buttonStackExpanded
-        }
-        set(newValue) {
-            Holder._buttonStackExpanded = newValue
-        }
+        get { return Holder._buttonStackExpanded }
+        set(newValue) { Holder._buttonStackExpanded = newValue }
     }
     
     var f1Button:UIButton {
-        get {
-            return Holder._f1Button
-        }
-        set(newValue) {
-            Holder._f1Button = newValue
-        }
+        get { return Holder._f1Button }
+        set(newValue) { Holder._f1Button = newValue }
     }
     
     var prevWeaponButton:UIButton {
-        get {
-            return Holder._prevWeaponButton
-        }
-        set(newValue) {
-            Holder._prevWeaponButton = newValue
-        }
+        get { return Holder._prevWeaponButton }
+        set(newValue) { Holder._prevWeaponButton = newValue }
     }
 
     var nextWeaponButton:UIButton {
-        get {
-            return Holder._nextWeaponButton
-        }
-        set(newValue) {
-            Holder._nextWeaponButton = newValue
-        }
+        get { return Holder._nextWeaponButton }
+        set(newValue) { Holder._nextWeaponButton = newValue }
     }
 
     var crouching:Bool {
-        get {
-            return Holder._crouching
-        }
-        set(newValue) {
-            Holder._crouching = newValue
-        }
+        get { return Holder._crouching }
+        set(newValue) { Holder._crouching = newValue }
     }
     
     var factor:CGFloat {
-        get {
-            return Holder._factor;
-        }
-        set (newValue) {
-            Holder._factor = newValue
-        }
+        get { return Holder._factor }
+        set(newValue) { Holder._factor = newValue }
     }
 
     @objc func fireButton(rect: CGRect) -> UIButton {
@@ -219,29 +151,23 @@ extension SDL_uikitviewcontroller {
         crouchButton.setTitle("CROUCH", for: .normal)
         crouchButton.backgroundColor = UIColor.gray
         crouchButton.addTarget(self, action: #selector(self.crouchPressed), for: .touchDown)
-//        crouchButton.addTarget(self, action: #selector(self.crouchReleased), for: .touchUpInside)
         crouchButton.alpha = 0.5
         return crouchButton
     }
     
     @objc func joyStick(rect: CGRect) -> JoyStickView {
         let size = CGSize(width: 100.0, height: 100.0)
-        let joystick1Frame = CGRect(origin: CGPoint(x: 50.0,
-                                                    y: (rect.height - size.height - 50.0)),
-                                    size: size)
+        let joystick1Frame = CGRect(origin: CGPoint(x: 50.0, y: (rect.height - size.height - 50.0)), size: size)
         joystickView = JoyStickView(frame: joystick1Frame)
         joystickView.delegate = self
-        
         joystickView.movable = false
         joystickView.alpha = 0.5
-        joystickView.baseAlpha = 0.5 // let the background bleed thru the base
-        joystickView.handleTintColor = UIColor.darkGray // Colorize the handle
+        joystickView.baseAlpha = 0.5
+        joystickView.handleTintColor = UIColor.darkGray
         return joystickView
     }
     
     @objc func buttonStack(rect: CGRect) -> UIStackView {
-        
-        
         expandButton = UIButton(type: .custom)
         expandButton.setTitle(" > ", for: .normal)
         expandButton.addTarget(self, action: #selector(self.expand), for: .touchUpInside)
@@ -283,8 +209,6 @@ extension SDL_uikitviewcontroller {
         quickLoadButton.alpha = 0
         quickLoadButton.isHidden = true
 
-        
-//        buttonStack = UIStackView(frame: CGRect(x: 20, y: 20, width: 30, height: 300))
         buttonStack = UIStackView(frame: .zero)
         buttonStack.frame.origin = CGPoint(x: 50, y: 50)
         buttonStack.translatesAutoresizingMaskIntoConstraints = false
@@ -292,13 +216,11 @@ extension SDL_uikitviewcontroller {
         buttonStack.spacing = 8.0
         buttonStack.alignment = .leading
         buttonStack.addArrangedSubview(expandButton)
-//        buttonStack.addArrangedSubview(tildeButton)
         buttonStack.addArrangedSubview(escapeButton)
         buttonStack.addArrangedSubview(quickSaveButton)
         buttonStack.addArrangedSubview(quickLoadButton)
 
         return buttonStack
-        
     }
     
     @objc func f1Button(rect: CGRect) -> UIButton {
@@ -326,103 +248,47 @@ extension SDL_uikitviewcontroller {
         return nextWeaponButton
     }
 
-    @objc func firePressed(sender: UIButton!) {
-        Key_Event(137, qboolean(1), qboolean(1))
-    }
+    @objc func firePressed(sender: UIButton!) { Key_Event(137, qboolean(1), qboolean(1)) }
+    @objc func fireReleased(sender: UIButton!) { Key_Event(137, qboolean(0), qboolean(1)) }
     
-    @objc func fireReleased(sender: UIButton!) {
-        Key_Event(137, qboolean(0), qboolean(1))
-    }
+    @objc func jumpPressed(sender: UIButton!) { Key_Event(32, qboolean(1), qboolean(1)) }
+    @objc func jumpReleased(sender: UIButton!) { Key_Event(32, qboolean(0), qboolean(1)) }
     
-    @objc func jumpPressed(sender: UIButton!) {
-        Key_Event(32, qboolean(1), qboolean(1))
-    }
-    
-    @objc func jumpReleased(sender: UIButton!) {
-        Key_Event(32, qboolean(0), qboolean(1))
-    }
-    
-    @objc func usePressed(sender: UIButton!) {
-        Key_Event(102, qboolean(1), qboolean(1))
-    }
-    
-    @objc func useReleased(sender: UIButton!) {
-        Key_Event(102, qboolean(0), qboolean(1))
-    }
+    @objc func usePressed(sender: UIButton!) { Key_Event(102, qboolean(1), qboolean(1)) }
+    @objc func useReleased(sender: UIButton!) { Key_Event(102, qboolean(0), qboolean(1)) }
     
     @objc func crouchPressed(sender: UIButton!) {
         crouching = !crouching
         Key_Event(99, crouching ? qboolean(1) : qboolean(0), qboolean(1))
     }
     
-//    @objc func crouchReleased(sender: UIButton!) {
-//        Key_Event(99, qboolean(0), qboolean(1))
-//    }
+    @objc func tildePressed(sender: UIButton!) {}
+    @objc func tildeReleased(sender: UIButton!) {}
     
-    @objc func tildePressed(sender: UIButton!) {
-//        Key_Event(32, qboolean(1), qboolean(1))
-    }
+    @objc func escapePressed(sender: UIButton!) { Key_Event(27, qboolean(1), qboolean(1)) }
+    @objc func escapeReleased(sender: UIButton!) { Key_Event(27, qboolean(0), qboolean(1)) }
     
-    @objc func tildeReleased(sender: UIButton!) {
-//        Key_Event(32, qboolean(0), qboolean(1))
-    }
+    @objc func quickSavePressed(sender: UIButton!) { Key_Event(150, qboolean(1), qboolean(1)) }
+    @objc func quickSaveReleased(sender: UIButton!) { Key_Event(150, qboolean(0), qboolean(1)) }
     
-    @objc func escapePressed(sender: UIButton!) {
-        Key_Event(27, qboolean(1), qboolean(1))
-    }
+    @objc func quickLoadPressed(sender: UIButton!) { Key_Event(153, qboolean(1), qboolean(1)) }
+    @objc func quickLoadReleased(sender: UIButton!) { Key_Event(153, qboolean(0), qboolean(1)) }
     
-    @objc func escapeReleased(sender: UIButton!) {
-        Key_Event(27, qboolean(0), qboolean(1))
-    }
+    @objc func f1Pressed(sender: UIButton!) { Key_Event(145, qboolean(1), qboolean(1)) }
+    @objc func f1Released(sender: UIButton!) { Key_Event(145, qboolean(0), qboolean(1)) }
     
-    @objc func quickSavePressed(sender: UIButton!) {
-        Key_Event(150, qboolean(1), qboolean(1))
-    }
+    @objc func prevWeaponPressed(sender: UIButton!) { Key_Event(183, qboolean(1), qboolean(1)) }
+    @objc func prevWeaponReleased(sender: UIButton!) { Key_Event(183, qboolean(0), qboolean(1)) }
     
-    @objc func quickSaveReleased(sender: UIButton!) {
-        Key_Event(150, qboolean(0), qboolean(1))
-    }
-    
-    @objc func quickLoadPressed(sender: UIButton!) {
-        Key_Event(153, qboolean(1), qboolean(1))
-    }
-    
-    @objc func quickLoadReleased(sender: UIButton!) {
-        Key_Event(153, qboolean(0), qboolean(1))
-    }
-    
-    @objc func f1Pressed(sender: UIButton!) {
-        Key_Event(145, qboolean(1), qboolean(1))
-    }
-    
-    @objc func f1Released(sender: UIButton!) {
-        Key_Event(145, qboolean(0), qboolean(1))
-    }
-    
-    @objc func prevWeaponPressed(sender: UIButton!) {
-        Key_Event(183, qboolean(1), qboolean(1))
-    }
-    
-    @objc func prevWeaponReleased(sender: UIButton!) {
-        Key_Event(183, qboolean(0), qboolean(1))
-    }
-    
-    @objc func nextWeaponPressed(sender: UIButton!) {
-        Key_Event(184, qboolean(1), qboolean(1))
-    }
-    
-    @objc func nextWeaponReleased(sender: UIButton!) {
-        Key_Event(184, qboolean(0), qboolean(1))
-    }
+    @objc func nextWeaponPressed(sender: UIButton!) { Key_Event(184, qboolean(1), qboolean(1)) }
+    @objc func nextWeaponReleased(sender: UIButton!) { Key_Event(184, qboolean(0), qboolean(1)) }
 
-    
     func Key_Event(_ key: Int32, _ down: qboolean, _ special: qboolean) {
         CL_KeyEvent(key, down, UInt32(Sys_Milliseconds()))
     }
 
     @objc func expand(_ sender: Any) {
         buttonStackExpanded = !buttonStackExpanded
-        
         UIView.animate(withDuration: 0.5) {
             self.expandButton.setTitle(self.buttonStackExpanded ? " < " : " > ", for: .normal)
             self.expandButton.alpha = self.buttonStackExpanded ? 1 : 0.5
@@ -448,53 +314,36 @@ extension SDL_uikitviewcontroller {
         self.nextWeaponButton.isHidden = hide
     }
     
-    open override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    open override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
+    open override func viewDidLoad() { super.viewDidLoad() }
+    open override func viewDidAppear(_ animated: Bool) { super.viewDidAppear(animated) }
 }
 
 extension SDL_uikitviewcontroller: JoystickDelegate {
     
     func handleJoyStickPosition(x: CGFloat, y: CGFloat) {
-
-        if y > 0.25 {
-            cl_joyscale_y.0 = Int32(abs(y) * 60)
-            Key_Event(132, qboolean(1), qboolean(1))
-            Key_Event(133, qboolean(0), qboolean(1))
-        } else if y < -0.25 {
-            cl_joyscale_y.1 = Int32(abs(y) * 60)
-            Key_Event(132, qboolean(0), qboolean(1))
-            Key_Event(133, qboolean(1), qboolean(1))
-        } else {
-            cl_joyscale_y.0 = 0
-            cl_joyscale_y.1 = 0
-            Key_Event(132, qboolean(0), qboolean(1))
-            Key_Event(133, qboolean(0), qboolean(1))
-        }
-        
-        if x > 0.25 {
-            cl_joyscale_x.0 = Int32(abs(y) * 20)
-            Key_Event(135, qboolean(1), qboolean(1))
-            Key_Event(134, qboolean(0), qboolean(1))
-        } else if x < -0.25 {
-            cl_joyscale_x.1 = Int32(abs(y) * 20)
-            Key_Event(135, qboolean(0), qboolean(1))
-            Key_Event(134, qboolean(1), qboolean(1))
-        } else {
-            cl_joyscale_x.0 = 0
-            cl_joyscale_x.1 = 0
-            Key_Event(135, qboolean(0), qboolean(1))
-            Key_Event(134, qboolean(0), qboolean(1))
-        }
-        
+        .
     }
 
     func handleJoyStick(angle: CGFloat, displacement: CGFloat) {
-//        print("angle: \(angle) displacement: \(displacement)")
+        
+        if displacement == 0 {
+            Key_Event(119, qboolean(0), qboolean(1)) // W
+            Key_Event(115, qboolean(0), qboolean(1)) // S
+            Key_Event(97,  qboolean(0), qboolean(1)) // A
+            Key_Event(100, qboolean(1), qboolean(0)) // D 
+            Key_Event(100, qboolean(0), qboolean(1)) // D
+            return
+        }
+        
+        
+        let radians = (180.0 - angle) * CGFloat.pi / 180.0
+        let dx = sin(radians) * displacement
+        let dy = cos(radians) * displacement
+        
+        
+        Key_Event(119, dy > 0.35 ? qboolean(1) : qboolean(0), qboolean(1)) // W
+        Key_Event(115, dy < -0.35 ? qboolean(1) : qboolean(0), qboolean(1)) // S
+        Key_Event(97,  dx < -0.35 ? qboolean(1) : qboolean(0), qboolean(1)) // A
+        Key_Event(100, dx > 0.35 ? qboolean(1) : qboolean(0), qboolean(1)) // D
     }
-    
 }
